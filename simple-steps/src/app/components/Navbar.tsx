e136171d24764476cd9d27e5293b29d3c491360f
Haroun Kassouri <harounkassouri368@gmail.com>
2024-11-26 17:33:05 +0000

    Basic Frontend and file setup

---
+++ simple-steps/src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+      <nav className="bg-blue-500 text-white p-4">
+        <div className="container mx-auto">
+          <h1 className="text-lg font-bold">Simple Steps</h1>
+        </div>
+      </nav>
+    );
+  };
+  
+export default Navbar;
+  

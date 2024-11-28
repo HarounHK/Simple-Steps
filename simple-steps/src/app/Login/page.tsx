f9bed440d2bfb1ae4b36aaf584daa3373890d690
Haroun Kassouri <harounkassouri368@gmail.com>
2024-11-28 00:10:12 +0000

    Added Routing

---
+++ simple-steps/src/app/Login/page.tsx
@@ -0,0 +1,13 @@
+import Login from "./Login";
+
+export default function LoginPage() {
+    return (
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-8">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">Welcome to Simple Steps</h1>
+        <p className="text-lg text-gray-700 mb-6">
+          <Login />
+        </p>
+      </div>
+    );
+  }
+  
